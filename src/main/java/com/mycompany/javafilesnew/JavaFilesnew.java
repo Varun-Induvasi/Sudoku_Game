511060a648edcf8cfff23bb51f1c46b9a5a01073
Varun-Induvasi <135617197+Varun-Induvasi@users.noreply.github.com>
2025-04-28 18:18:29 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/javafilesnew/JavaFilesnew.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javafilesnew;
+
+/**
+ *
+ * @author induv
+ */
+public class JavaFilesnew {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
